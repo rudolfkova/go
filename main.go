c21484c4c6038717c60c3cc90373eff7c3d4b7de
Грач <smirnov_niki@bk.ru>
2024-09-01 23:47:53 +0300

    v7

--- main.go
+++ main.go
@@ -2,19 +2,13 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/rudolfkova/go/shape"
 )
 
-type Circle struct {
-	area float64
-}
-
 func main() {
-	circle := Circle{Radius: 5}
-	rectangle := Rectangle{Width: 4, Height: 3}
+	var circle shape.Circle
+	circle.Radius = 10
+	fmt.Println(circle.Area())
 
-	fmt.Println("Circle area:", circle.Area())
-	fmt.Println("Circle perimeter:", circle.Perimeter())
-	fmt.Println("Rectangle area:", rectangle.Area())
-	fmt.Println("Rectangle perimeter:", rectangle.Perimeter())
 }
